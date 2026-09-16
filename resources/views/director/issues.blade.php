d6419e1d24999a98db42c82bc1ab28ba662fd942
Andhika-d <andhikadf24@gmail.com>
2026-09-16 13:27:09 +0700

    fix: tidy issue timeline labels and localize relative time

--- resources/views/director/issues.blade.php
+++ resources/views/director/issues.blade.php
@@ -88,17 +88,17 @@
                         </p>
                         @endif
 
-                        <div class="mt-3 flex flex-wrap gap-x-6 gap-y-1 text-xs text-slate-400">
+                        <div class="mt-3 flex flex-wrap gap-x-6 gap-y-1.5 text-xs text-slate-400">
                             @if($startedAt)
-                            <span>Proses tertunda sejak&nbsp;<strong class="font-semibold text-slate-600">{{ $startedAt->translatedFormat('d M Y, H:i') }}</strong></span>
+                            <span class="whitespace-nowrap">Proses tertunda sejak&nbsp;<strong class="font-semibold text-slate-600">{{ $startedAt->translatedFormat('d M Y, H:i') }}</strong></span>
                             @endif
                             @if($issue->detected_at)
-                            <span>Deteksi pertama {{ $issue->detected_at->translatedFormat('d M Y') }}</span>
+                            <span class="whitespace-nowrap">Deteksi pertama&nbsp;<strong class="font-semibold text-slate-600">{{ $issue->detected_at->translatedFormat('d M Y') }}</strong></span>
                             @endif
                             @if($issue->isActive())
-                            <span>Terakhir terdeteksi {{ $issue->last_seen_at?->diffForHumans() }}</span>
+                            <span class="whitespace-nowrap">Terakhir terdeteksi&nbsp;<strong class="font-semibold text-slate-600">{{ $issue->last_seen_at?->locale('id')->diffForHumans() }}</strong></span>
                             @elseif($issue->resolved_at)
-                            <span>Selesai {{ $issue->resolved_at->translatedFormat('d M Y, H:i') }}</span>
+                            <span class="whitespace-nowrap">Selesai&nbsp;<strong class="font-semibold text-slate-600">{{ $issue->resolved_at->translatedFormat('d M Y, H:i') }}</strong></span>
                             @endif
                         </div>
                     </div>
